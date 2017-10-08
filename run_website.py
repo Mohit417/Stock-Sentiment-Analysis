6f2fa293e858bbff6b96342addf384ec6dd6d386
Mohit Katyal <mohit417@berkeley.edu>
2017-10-08 08:39:51 -0700

    insert_company.html creatd

--- run_website.py
+++ run_website.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
    return render_template("insert_company.html")
 
+@app.route("/companyName=<cName>")
+def classify(cName):
+    return render_template('company.html',name = cName)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
